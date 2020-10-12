f006bd49df8b2b485aadd2cc3369e0820e4b1027
Victor Seara <victorseara.me@gmail.com>
2020-10-12 14:16:12 -0300

    Getting started with styled-components

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -2,23 +2,22 @@ import React from "react";
 import { Box, Divider, Typography } from "@material-ui/core";
 import iphone from "../../assets/img/iphone.png";
 import { Star } from "@material-ui/icons";
+import {Page,PageContainer, ProductContainer, ProductImageContainer, ProductImage, ProductContent, Title} from './styles'
+import {iphone as product} from '../../data'
 
 export default function Product() {
   return (
-    <Box
-      style={{ background: "#fdfdfd" }}
-      display="flex"
-      flex={1}
-      margin="3rem"
-    >
-      <Box display="flex">
-        <Box>
-          <img src={iphone} width="400px" height="400px" />
-        </Box>
-        <Box displa="flex" flexDirection="column">
-          <Typography>
-            iPhone 11 64GB Preto iOS 4G Câmera 12MP - Apple
-          </Typography>
+    <Page>
+      <PageContainer>
+      <Box>
+      <ProductContainer>
+        <ProductImageContainer>
+          <ProductImage src={iphone} width="400px" height="400px" />
+        </ProductImageContainer>
+        <ProductContent>
+          <Title>
+           {product.title}
+          </Title>
           <Box display="flex" margin=" 2rem 0 2rem 0">
             <Box marginRight="30px" display="flex">
               <Star />
@@ -26,19 +25,15 @@ export default function Product() {
               <Star />
               <Star />
               <Star />
-              <Typography>(55)</Typography>
+            <Typography>({product.numberOfAvaliations})</Typography>
             </Box>
             <Box>
-              <Typography>(Cód.1611315933)</Typography>
+              <Typography>(Cód.{product.code})</Typography>
             </Box>
           </Box>
           <Box margin=" 2rem 0 2rem 0">
             <Typography>
-              Tudo. Na medida certa. O iPhone 11 tem novo sistema de câmera
-              dupla para capturar mais do que você vê e ama. Ele vem com o chip
-              mais rápido em um smartphone. E bateria para o dia todo: você faz
-              muito e recarrega pouco. Também tem a melhor qualidade de vídeo em
-              um celular. Assim, suas memórias ficam inesquecíveis de verdade.
+              {product.description}
             </Typography>
           </Box>
           <Box display="flex" flexDirection="column">
@@ -50,8 +45,10 @@ export default function Product() {
               política de troca e devolução
             </Typography>
           </Box>
-        </Box>
+        </ProductContent>
+      </ProductContainer>
       </Box>
-    </Box>
+      </PageContainer>
+    </Page>
   );
 }
---
+++ src/pages/Product/styles.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import {Box,Typography} from '@material-ui/core';
+
+export const Page = styled(Box)`
+    display: flex; 
+    flex: 1; 
+    flex-direction: column;
+    background: #FAFAFA;
+`;
+
+export const PageContainer = styled(Box)`
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    margin: 0 12rem;
+`;
+
+
+export const ProductContainer = styled(Box)`
+    display: flex;
+    background-color: #FFF;
+    padding: 12rem 0;
+    max-width: 60%; 
+    border-radius: .5rem;
+    box-shadow: 2px 0 12px -6px rgba(0,0,0,0.4);
+
+`; 
+
+export const ProductImageContainer = styled(Box)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+`;
+
+export const ProductImage = styled('img')`
+    height: 600px;
+    object-fit: cover;
+
+`; 
+
+export const ProductContent = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    padding: 0 4rem;
+
+`;
+
+export const Title = styled(Typography)`
+    font-size: 28px;
+    font-weight: bold;
+`;
